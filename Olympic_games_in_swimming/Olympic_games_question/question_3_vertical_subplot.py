7329e0e49723a3e7dc50b3271a50a60b684cb55f
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-06-12 15:55:23 +0300

    Adding the function data

--- Olympic_games_in_swimming/Olympic_games_question/question_3_vertical_subplot.py
+++ Olympic_games_in_swimming/Olympic_games_question/question_3_vertical_subplot.py
@@ -1,9 +1,39 @@
-import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatch
-import numpy as np
+import pandas as pd
 
+# **************************************************************************************************************
+# Function  name: prepering_the_data_for_vertical_subplots
+# input:
+# return value: Converting  the values to seconds with two decimal place
+# ****************************************************************************************************************
+def prepering_the_data_for_vertical_subplots(df):
+    data_without_na = df.dropna(how='all')
+    cleaner_df = data_without_na.dropna(subset=['Results'])
+    # Removing rows with the values : 'Disqualified','Did not start','Did not finish'
+    removing_words = ['Disqualified', 'Did not start', 'Did not finish', '36.4est']
+    final_clean_table = cleaner_df[~cleaner_df['Results'].isin(removing_words)]
+    # Retrieving the 2 data for male & female separately - after 3 constraints:
+    final_table_male = final_clean_table.loc[
+        (final_clean_table["Relay?"] == 1) & (final_clean_table['Gender'] == 'Men') & (
+                final_clean_table['Distance (in meters)'] == '4x100')]
+    final_table_female = final_clean_table.loc[
+        (final_clean_table["Relay?"] == 1) & (final_clean_table['Gender'] == 'Women') & (
+                final_clean_table['Distance (in meters)'] == '4x100')]
+    print('*')
+    final_table_female_sorted = final_table_female['Year'].unique().tolist()
+    sorted_years_list = sorted(final_table_female_sorted, reverse=False)
+    print('*')
+    for olympic_year in sorted_years_list:
+        # mini_df_men = final_table_male[final_table_male['Year'] == olympic_year] # men started only in 1960
+        mini_df_women = final_table_female[final_table_female['Year'] == olympic_year]
+        first_3_places = [1, 2, 3]
+        final_women_table = mini_df_women[mini_df_women['Rank'].isin(first_3_places)]
+        final_women_table = final_women_table.reset_index()
 
+        gold_annotation = final_women_table.loc[1, 'Team']
+        silver_annotation = final_women_table.loc[2, 'Team']
+        bronze_annotation = final_women_table.loc[3, 'Team']
+        print('*')
 
 
 # **************************************************************************************************************
@@ -12,7 +42,6 @@ import numpy as np
 # return value: Converting  the values to seconds with two decimal place
 # ****************************************************************************************************************
 def plotting_subplot_for_freestyle_vs_medley_relay():
-
     plt.figure(figsize=[14, 10])
     fig, all_4_axis = plt.subplots(nrows=1, ncols=4, sharey=True)  # 4 plots
     # plt.style.use('ggplot')
@@ -22,7 +51,7 @@ def plotting_subplot_for_freestyle_vs_medley_relay():
     all_4_axis[2].set_title('Men', fontsize=14, fontname='Franklin Gothic Medium Cond')
     all_4_axis[3].set_title('Women', fontsize=14, fontname='Franklin Gothic Medium Cond')
 
-    all_4_axis[0].barh(['Bronze', 'Silver', 'Gold'], [2026493, 710887, 476658], color='Gray') # width
+    all_4_axis[0].barh(['Bronze', 'Silver', 'Gold'], [2026493, 710887, 476658], color='Gray')  # width
     all_4_axis[1].barh(['Bronze', 'Silver', 'Gold'], [2026493, 710887, 476658], color='Gray')
     all_4_axis[2].barh(['Bronze', 'Silver', 'Gold'], [2026493, 710887, 476658], color='Gray')
     all_4_axis[3].barh(['Bronze', 'Silver', 'Gold'], [2026493, 710887, 476658], color='Gray')
@@ -34,26 +63,16 @@ def plotting_subplot_for_freestyle_vs_medley_relay():
     plt.show()
 
 
+
+
+
 if __name__ == '__main__':
 
     pd.set_option('display.max_rows', 5000)
     df = pd.read_csv('../Data/Olympic_Swimming_1912to2020.csv')
 
-    #plotting_subplot_for_freestyle_vs_medley_relay()
+    prepering_the_data_for_vertical_subplots(df)
+    # plotting_subplot_for_freestyle_vs_medley_relay()
     print('*')
 
-    data_without_na = df.dropna(how='all')
-    cleaner_df = data_without_na.dropna(subset=['Results'])
-    # Removing rows with the values : 'Disqualified','Did not start','Did not finish'
-    removing_words = ['Disqualified', 'Did not start', 'Did not finish', '36.4est']
-    final_clean_table = cleaner_df[~cleaner_df['Results'].isin(removing_words)]
 
-    # Retrieving the 2 data for male & female separately - after 3 constraints:
-    final_table_male = final_clean_table.loc[(final_clean_table["Relay?"] == 1) & (final_clean_table['Gender'] == 'Men') & (final_clean_table['Distance (in meters)'] == '4x100')]
-    final_table_female = final_clean_table.loc[(final_clean_table["Relay?"] == 1) & (final_clean_table['Gender'] == 'Women') & (final_clean_table['Distance (in meters)'] == '4x100')]
-    print('*')
-
-    final_table_female_sorted= final_table_female['Year'].unique().tolist()
-    #np.sort(final_table_female_sorted, axis=-1, kind=None, order=None)
-
-    #for olympic_year in final_table_female_sorted
