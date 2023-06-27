3e7658509d988567a16de3595169e7c590a00229
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-06-27 19:41:57 +0300

    Starting with the avg dot plot of each country - Question 4

---
+++ Olympic_games_in_swimming/Olympic_games_question/question_4_avg_dot_plot.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# **************************************************************************************************************
+# Function  name: preparing_the_data_for_the_dot_plot
+# input:
+# return value:
+# ****************************************************************************************************************
+
+def preparing_the_data_for_the_dot_plot(mini_df_team):
+    # Retrieving -  3 first place
+    first_3_places = [1, 2, 3]
+    final_medals_table = mini_df_team[mini_df_team['Rank'].isin(first_3_places)]
+    final_medals_table.reset_index(inplace=True)
+    print('*')
+    list_of_number_of_medals_for_the_team = []
+    grouping_by_year = final_medals_table.groupby('Year')
+    for year_olympic, mini_df_year in grouping_by_year:
+        print(year_olympic)
+        print(mini_df_year)
+        medals_specific_year = mini_df_year.shape[0]
+        print('*')
+
+        # list_of_number_of_medals_for_the_team.append()
+        list_of_number_of_medals_for_the_team.append(medals_specific_year)
+    print('*')
+    avg_of_the_team_over_the_years = np.mean(list_of_number_of_medals_for_the_team) # In order to create the vertical line
+    print('*')
+
+    return avg_of_the_team_over_the_years
+
+
+if __name__ == '__main__':
+
+    pd.set_option('display.max_rows', 5000)
+    df = pd.read_csv('../Data/Olympic_Swimming_1912to2020.csv')
+    print('*')
+
+    # Cleaning_the_data:
+    data_without_na = df.dropna(how='all')
+    cleaner_df = data_without_na.dropna(subset=['Results'])
+    # Removing rows with the values : 'Disqualified','Did not start','Did not finish'
+    removing_words = ['Disqualified', 'Did not start', 'Did not finish', '36.4est']
+    final_clean_table = cleaner_df[~cleaner_df['Results'].isin(removing_words)]
+
+    list_of_teams = ['USA','AUS','GBR','JPN','GER','CAN','GDR','HUN']
+    for team_names in list_of_teams:
+        mini_df_team = final_clean_table[final_clean_table['Team'] == team_names]
+        preparing_the_data_for_the_dot_plot(mini_df_team)
+        print('*')
