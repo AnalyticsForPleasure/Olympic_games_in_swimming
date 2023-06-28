d9c2931c2048755a0daed6848acf26cd76b429fe
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-06-28 06:29:34 +0300

    Strating with the visualizations chart for each team - Q 4

--- Olympic_games_in_swimming/Olympic_games_question/question_4_avg_dot_plot.py
+++ Olympic_games_in_swimming/Olympic_games_question/question_4_avg_dot_plot.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import seaborn as sns
 
 # **************************************************************************************************************
 # Function  name: preparing_the_data_for_the_dot_plot
@@ -37,6 +38,39 @@ def preparing_the_data_for_the_dot_plot(mini_df_team):
     print('*')
     return avg_of_the_team_over_the_years , final_table
 
+# **************************************************************************************************************
+# Function  name: creating_the_plot_chart_of_each_teams
+# input:
+# return value:
+# ****************************************************************************************************************
+def creating_the_dot_chart_of_each_teams(avg_of_the_team_over_the_years,final_table):
+
+    #sns.catplot(data=tips, x="day", y="total_bill", jitter=False)
+
+    #https://www.machinelearningplus.com/plots/top-50-matplotlib-visualizations-the-master-plots-python/#17.-Dot-Plot
+    # Prepare Data
+    df_raw = pd.read_csv("https://github.com/selva86/datasets/raw/master/mpg_ggplot2.csv")
+    df = df_raw[['cty', 'manufacturer']].groupby('manufacturer').apply(lambda x: x.mean())
+    df.sort_values('cty', inplace=True)
+    df.reset_index(inplace=True)
+
+    # Draw plot
+    fig, ax = plt.subplots(figsize=(16,10), dpi= 80)
+    ax.hlines(y=df.index, xmin=11, xmax=26, color='gray', alpha=0.7, linewidth=1, linestyles='dashdot')
+    ax.scatter(y=df.index, x=df.cty, s=75, color='firebrick', alpha=0.7)
+
+    # Title, Label, Ticks and Ylim
+    ax.set_title('Dot Plot for Highway Mileage', fontdict={'size':22})
+    ax.set_xlabel('Miles Per Gallon')
+    ax.set_yticks(df.index)
+    ax.set_yticklabels(df.manufacturer.str.title(), fontdict={'horizontalalignment': 'right'})
+    ax.set_xlim(10, 27)
+    plt.show()
+
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -55,5 +89,6 @@ if __name__ == '__main__':
 
     for team_names in list_of_teams:
         mini_df_team = final_clean_table[final_clean_table['Team'] == team_names]
-        preparing_the_data_for_the_dot_plot(mini_df_team)
+         preparing_the_data_for_the_dot_plot(mini_df_team)
+        creating_the_dot_chart_of_each_teams()
         print('*')
