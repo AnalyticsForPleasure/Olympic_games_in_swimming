a1e2f7abe7a66b86d7b0b93249bc6d1e46eefe6b
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-07-04 07:44:12 +0300

    Question  5

---
+++ Olympic_games_in_swimming/Olympic_games_question/question_5/home_court_gives_edge.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+# **************************************************************************************************************
+# Function  name:
+# input: How home field adventage gives O;ympic hpst countries an Edge?
+# return value:
+# ****************************************************************************************************************
+
+
+if __name__ == '__main__':
+
+    pd.set_option('display.max_rows', 5000)
+    df = pd.read_csv('../../Data/Olympic_Swimming_1912to2020.csv')
+    # df = pd.read_csv('/home/shay_diy/PycharmProjects/Olympic_games/data/Olympic_Swimming.csv')
+
+
+
+
