c2301aceb7b1ea52e6e175be2224b4a56a55aadf
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-07-08 04:44:04 +0300

    swimming_pool_animation - need to make it dynamic

--- Olympic_games_in_swimming/Olympic_games_question/question_7/swimming_pool_animation.py
+++ Olympic_games_in_swimming/Olympic_games_question/question_7/swimming_pool_animation.py
@@ -15,20 +15,31 @@ if __name__ == '__main__':
 
     pd.set_option('display.max_rows', 5000)
     df = pd.read_csv('../../Data/Olympic_Swimming_1912to2020.csv')
-    # df = pd.read_csv('/home/shay_diy/PycharmProjects/Olympic_games/data/Olympic_Swimming.csv')
+
     print('*')
 
-    im = image.imread('C:\Users\Gil\PycharmProjects\Olympic_games_in_swimming_1\Olympic_games_in_swimming\Olympic_games_question\question_1\Cleveland_dot_plot_AUS.jpg')
+####################################################################################################################################################################
 
-    fig, ax = plt.subplots()
-    ax.grid()
-    #ax.plot('Year','earnings ($ million)',data=lebron_james)
-    ax.set_title("LeBron James earnings in US$(millions)")
-    fig.figimage(im, 60, 40,cmap='ocean', alpha=.2)
-    plt.show()
-    plt.figimage(im, xo=1000, yo=135)
+# Reading image using imread
+image = plt.imread("C:/Users/Gil/PycharmProjects/Olympic_games_in_swimming_1/Olympic_games_in_swimming/Data/swimming_pool_photo.png")
 
+# Create a sample dataset
+data = {'Year': [2015, 2016, 2017, 2018, 2019],
+        'earnings ($ million)': [100, 150, 200, 180, 220]}
 
+df = pd.DataFrame(data)
 
-    #img = plt.imread("C:\Users\Gil\PycharmProjects\Olympic_games_in_swimming_1\Olympic_games_in_swimming\Olympic_games_question\question_7\swimming_pool_image.png")
-    print('*')
+fig, ax = plt.subplots()
+
+ax.grid()
+ax.barh('Year', 'earnings ($ million)', data=df,color ='black')  # Use barh for horizontal bar plot
+
+ax.set_xlabel('Earnings ($ million)')
+ax.set_ylabel('Swimming Teams')
+
+ax.set_title("LeBron James earnings in US$ (millions)")
+fig.figimage(image, 50, 50, cmap='ocean', alpha=0.55)
+plt.show()
+
+# Displaying the read image
+plt.imshow(image)
