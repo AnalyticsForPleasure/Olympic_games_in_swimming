df02854174bb2b7de392c1aba4556bcee4d68328
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-07-13 16:30:32 +0300

    Adding Dict - location_city

--- Olympic_games_in_swimming/Olympic_games_question/question_5/home_court_gives_edge.py
+++ Olympic_games_in_swimming/Olympic_games_question/question_5/home_court_gives_edge.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # **************************************************************************************************************
 # Function  name:
-# input: How home field advantage gives O;ympic hpst countries an Edge?
+# input: How home field advantage gives Olympic hpst countries an Edge?
 # return value:
 # ****************************************************************************************************************
 
@@ -21,6 +21,53 @@ if __name__ == '__main__':
     # Removing rows with the values : 'Disqualified','Did not start','Did not finish'
     removing_words = ['Disqualified', 'Did not start', 'Did not finish', '36.4est']
     final_clean_table = cleaner_df[~cleaner_df['Results'].isin(removing_words)]
+    print('*')
+
+    Unique_Location = list(final_clean_table['Location'].unique())
+    #['Tokyo', 'Rio', 'London', 'Beijing', 'Athens', 'Sydney', 'Atlanta', 'Barcelona', 'Seoul', 'Angeles', 'Moscow', 'Montreal', 'Munich', 'City', 'Rome', 'Melbourne', 'Helsinki', 'Berlin', 'Amsterdam', 'Paris', 'Antwerp', 'Stockholm']#
+    Unique_Team = list(final_clean_table['Team'].unique())
+    # ['ROC', 'USA', 'ITA', 'CHN', 'ESP', 'AUS', 'ROU', 'GBR', 'NED', 'BLR', 'HUN', 'SUI', 'POL', 'GUA', 'BUL', 'FRA', 'KOR', 'UKR', 'GER', 'AUT', 'JPN', '', 'SWE', '', 'BRA', 'LTU', 'NZL', 'TUN', 'GRE', 'CAN', 'ISR', 'IRL', 'HKG', 'BEL', 'CZE', 'DEN', 'RUS', 'KAZ', 'ISL', 'JAM', 'SGP', 'NOR', 'ZIM', 'NZ', 'NL', 'NLD', 'SRB', 'CUB', 'ZAF', 'TTO', 'BAH', 'VEN', 'KEN', 'PNG', 'SLO', 'CRO', 'SVK', 'ALG', 'ARG', 'CRC', 'PUR', 'MDA', 'BAR', 'EUN', 'SUR', 'Unified Team ', 'TCH', 'URS', 'GDR', 'FRG', 'YUG', 'SUN', 'BGR', 'COL', 'POR', 'CHE', 'MEX', 'ECU', 'PER', 'URU', 'EGY', 'PHI', 'LUX', 'ANZ']
+    print('*')
+
+    # Create a dictionary of the location of the Olympic took place and team
+    dict_location_city = {
+        "Tokyo": "JPN",
+        "Rio": "Mustang",
+        "Atlanta":'USA',
+        "London": "GBR",
+        "Paris": "FRA",
+        "Sydney":"AUS",
+        "Barcelona": "ESP",
+        "Melbourne": "AUS",
+        "Montreal":"CAN",
+        "Angeles":"USA",
+        "London": "GBR",
+        "Munich": "GRE",
+        "Berlin": "GRE",
+        "Athens":"GRE",
+        "Seoul":"", # South Korea
+        "Stockholm": "GRE",
+        "Athens":"GRE",
+        "Amsterdam":"NED",
+        "Helsinki":"FIN",
+        "Moscow":"RSA",
+        "Seoul":"FIN",
+        "Antwerp":"BUL",
+        "Rome":"GRE"}
+
+
+
+
+
+
+
+
+
+
+
+['USA','AUS','GBR','JPN','GER','CAN','GDR','HUN']
+    # }
+
 
 
 
