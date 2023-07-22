adebe68d734dca4c31a4aeb4b3b37b00545e2c36
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-07-22 08:10:20 +0300

    Adding each image to the bars inside the bar chart

--- Olympic_games_in_swimming/Olympic_games_question/question_5/home_court_gives_edge.py
+++ Olympic_games_in_swimming/Olympic_games_question/question_5/home_court_gives_edge.py
@@ -2,6 +2,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import dataframe_image as dfi
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+
 
 # **************************************************************************************************************
 # Function  name: relevant_columns_highlighter
@@ -97,11 +99,51 @@ def retrieving_the_number_of_medals_with_home_court_advantage(final_clean_table)
     print('*')
     #C:\Users\Gil\PycharmProjects\Olympic_games_in_swimming_1\Olympic_games_in_swimming\Olympic_games_question\question_5\output_image
     return final_table
+###############################################################################################################################################
+# Function  name: need to connect this part to the function up here
+# input:
+# return value:
+###############################################################################################################################################
+# # Step 1: Prepare the bar chart data and labels
+# data = [10, 12, 15, 14, 12, 18]
+# #labels = ['Bar 1', 'Bar 2', 'Bar 3', 'Bar 4', 'Bar 5', 'Bar 6']
+# x = [0,1, 2, 3, 4, 5]
+#
+# # Step 2: Prepare the small images (Replace 'image1.png', 'image2.png', and 'image3.png' with your actual image file paths)
+# image_paths = ['beijig_2008.png', 'london_2012.png', 'sydney_2000.png','stockholm_1956.png','tokyo_2020.png','los_angeles_1984.png']
+# images = [plt.imread(path) for path in image_paths]
+#
+# plt.figure(facecolor='gainsboro')
+# # Step 3: Create the bar chart
+# #plt.bar(range(len(data)), data, tick_label=labels)
+#
+# # Step 4: Add the images under each bar
+# for i, image in enumerate(images):
+#     imagebox = OffsetImage(image, zoom=0.45)  # Adjust the 'zoom' parameter to scale the image size
+#     ab = AnnotationBbox(imagebox, (i, 0), xybox=(0, -60), xycoords='data', boxcoords="offset points", frameon=False)
+#     plt.gca().add_artist(ab)
+#
+#
+#
+#
+# #bar plot
+# plt.bar(x, height = data, width = 0.6)
+#
+# # Set the title and labels for the chart
+# plt.title("Number of medals with home court advantage")
+# #plt.xlabel("Home Court")
+# plt.ylabel("Values")
+#
+#
+# # Show the plot
+# plt.show()
+
+
 
 
 # **************************************************************************************************************
 # Function  name:
-# input: How home field advantage gives Olympic hpst countries an Edge?
+# input:
 # return value:
 # ****************************************************************************************************************
 if __name__ == '__main__':
