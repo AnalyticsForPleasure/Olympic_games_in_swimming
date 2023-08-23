8d719daea61ae492f3325cd12ffbb4ce8132b0b0
AnalyticsForPleasure <shaycohen2003@gmail.com>
2023-08-23 19:08:26 +0300

    update - and saperating the 3 madals df into 3 seperate tables : Gold, Silver and bronze

--- Olympic_games_in_swimming/Olympic_games_question/question_5/home_court_gives_edge.py
+++ Olympic_games_in_swimming/Olympic_games_question/question_5/home_court_gives_edge.py
@@ -62,38 +62,52 @@ def retrieving_the_number_of_medals_with_home_court_advantage(final_clean_table)
     List_of_best_home_courts = ['London', 'Munich', 'Seoul', 'Stockholm', 'Sydney', 'Tokyo', 'Beijing', 'Moscow',
                                 'Melbourne', 'Berlin', 'Angeles', 'Atlanta']
     List_of_country_code = ['GBR', 'GRE', 'KOR', 'SWE', 'AUS', 'JPN', 'CHN', 'RSA', 'AUS', 'GRE', 'USA', 'USA']
-    first_3_places = [1, 2, 3]
+    #first_3_places = [1, 2, 3]
+    first_place_gold = [1]
+    #second_place_silver = [2]
     # 1934 - Angeles - 10 medals
     for home_court_location, code_country, olympic_year_before in zip(List_of_best_home_courts, List_of_country_code,
                                                                       list_of_years_minus_4):
         # Home court info:
         home_court_data = final_clean_table[final_clean_table['Location'] == home_court_location]
         home_court_data_code_country = home_court_data[home_court_data['Team'] == code_country]
-        home_court_data = home_court_data_code_country[home_court_data_code_country['Rank'].isin(first_3_places)]
+        home_court_data = home_court_data_code_country[home_court_data_code_country['Rank'].isin(first_place_gold)] # first_3_places
         number_of_medals = home_court_data.shape[0]
         list_number_of_medals_home_court_advantage.append(number_of_medals)
 
         # Before home court (One Olympic games before) - number of medals:
         before_home_court_data = final_clean_table.loc[final_clean_table['Year'] == olympic_year_before, :]
-        before_home_court_data_code_country = before_home_court_data.loc[before_home_court_data['Team'] == code_country,
-                                              :]
+        before_home_court_data_code_country = before_home_court_data.loc[before_home_court_data['Team'] == code_country, :]
         home_court_data_3_places = before_home_court_data_code_country.loc[
-            before_home_court_data_code_country['Rank'].isin(first_3_places)]
+            before_home_court_data_code_country['Rank'].isin(first_place_gold)]
         number_of_medals_before = home_court_data_3_places.shape[0]
         list_number_of_medals_before_home_court_advantage.append(number_of_medals_before)
         print('*')
     final_table = pd.DataFrame({'Host City': List_of_best_home_courts,
-                                'Country code': List_of_country_code,
+                                #'Country code': List_of_country_code,
                                 'Medals before getting home court advantage': list_number_of_medals_before_home_court_advantage,
                                 'Medals with home court advantage': list_number_of_medals_home_court_advantage})
-    final_table.at[10, 'Medals with home court advantage'] = 34
+
+
+    # Initialize a list to store the indices of rows to be dropped
+    rows_to_drop = []
+
+    for index, row in final_table.iterrows():
+        if row['Medals before getting home court advantage'] == 0 and row['Medals with home court advantage'] == 0:
+            rows_to_drop.append(index)
+
+    # Drop the rows using the list of indices
+    final_table = final_table.drop(rows_to_drop)
+    print('*')
+        #if final_table.loc[,'Medals before getting home court advantage'] == 0 &
+    #final_table.at[10, 'Medals with home court advantage'] = 34
 
 
     #result = final_table.style.apply(func=relevant_columns_highlighter, subset=['Host Cities of the Olympic Games']).hide_index()
     #dfi.export(result,'Users\Gil\PycharmProjects\Olympic_games_in_swimming_1\Olympic_games_in_swimming\Olympic_games_question\question_5\output_image.png')
     #dfi.export(result, filename='output_images/home_court_image.png')
 
-    dfi.export(final_table, 'home_court_image.png')
+    dfi.export(final_table, 'home_court_gold_image.png')
 
     #
     print('*')
